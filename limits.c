900039e05b0e94be95d94a207feec85940395f95
ronan207 <12341@holbertonstudents.com>
2025-11-28 15:51:58 +0100

    i have delte two file on my repository " q git commit -m "i have delete two file on my repository"

--- limits.c
+++ limits.c
@@ -9,12 +9,15 @@
  */
 
 int write_char(char *ch)
-
-        if (ch == NULL)
 {
-                return (0);
+	if (ch == NULL)
+	{
+		return (1);
 
-        write(1, &ch,1);
+	}
+	write(1, &ch,1);
+
+		return (0);
 }
 
 
